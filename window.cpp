7115976b1f08f75e60964327c2c74a4132c30006
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-03-13 16:55:13 +0100

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -1,20 +1,21 @@
+
 #include <windows.h>
 #include <iostream>
 LRESULT CALLBACK WndProc(HWND app, UINT msg, WPARAM wParam, LPARAM lParam);
 int WINAPI wWinMain(
 	HINSTANCE hInstance,
-	HINSTANCE previnstance,	
+	HINSTANCE previnstance,
 	LPTSTR cmdline,
 	int cmdshow
-) {	
-	WNDCLASS window;
+) {
+	WNDCLASS window = {};
 	window.lpfnWndProc = WndProc;
 	window.lpszClassName = L"yeetboi2";
 	window.hInstance = hInstance;
 	RegisterClass(&window);
 	HWND app = CreateWindowEx(0, L"yeetboi2", L"yeetboi", WS_OVERLAPPEDWINDOW, 400, 400, 400, 400, NULL, NULL, hInstance, NULL);
 	ShowWindow(app, cmdshow);
-	while (true){
+	while (true) {
 
 	}
 }
