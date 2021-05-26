0c379f0758c6771208a26ed655613e5f90be0bf3
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-05-26 16:24:48 +0200

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -1,7 +1,5 @@
-
 #include <windows.h>
-#include <iostream>
-LRESULT CALLBACK WndProc(HWND app, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND app, UINT msg, WPARAM wparam, LPARAM lparam);
 int WINAPI wWinMain(
 	HINSTANCE hInstance,
 	HINSTANCE previnstance,
@@ -15,10 +13,14 @@ int WINAPI wWinMain(
 	RegisterClass(&window);
 	HWND app = CreateWindowEx(0, L"yeetboi2", L"yeetboi", WS_OVERLAPPEDWINDOW, 400, 400, 400, 400, NULL, NULL, hInstance, NULL);
 	ShowWindow(app, cmdshow);
+	PAINTSTRUCT verf;
+	HDC pixel = BeginPaint(app, &verf);
+	HBRUSH redkwast = CreateSolidBrush(RGB(255, 0, 0));
+	HBRUSH blackkwast = CreateSolidBrush(RGB(0, 0, 0));
 	while (true) {
 
 	}
 }
-LRESULT CALLBACK WndProc(HWND app, UINT msg, WPARAM wParam, LPARAM lParam) {
-	return 0;
+LRESULT CALLBACK WndProc(HWND app, UINT msg, WPARAM wparam, LPARAM lparam) {
+	return DefWindowProc(app, msg, wparam, lparam);
 }
