3831252855462533d61ba9072c1b4024ed231625
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-06-08 14:46:56 +0200

    Update global.h

--- snek/global.h
+++ snek/global.h
@@ -1,8 +1,9 @@
+
 #pragma once
 #include <windows.h>
 void the_game(HDC pixel);
-HDC init_game(HWND app,char food);
-char network_init();
+HDC init_game(HWND app,int food);
+int network_init();
 int socket_data(char data);
 int new_food();
 extern char wormID;
