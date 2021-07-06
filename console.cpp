91e479b9cf867f306936cb937539c5261c585ac7
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-07-06 09:51:19 +0200

    Create console.cpp

---
+++ console.cpp
@@ -0,0 +1,19 @@
+AllocConsole();
+
+	HANDLE ConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	int SystemOutput = _open_osfhandle(intptr_t(ConsoleOutput), _O_TEXT);
+	FILE* COutputHandle = _fdopen(SystemOutput, "w");
+
+	HANDLE ConsoleError = GetStdHandle(STD_ERROR_HANDLE);
+	int SystemError = _open_osfhandle(intptr_t(ConsoleError), _O_TEXT);
+	FILE* CErrorHandle = _fdopen(SystemError, "w");
+
+	HANDLE ConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+	int SystemInput = _open_osfhandle(intptr_t(ConsoleInput), _O_TEXT);
+	FILE* CInputHandle = _fdopen(SystemInput, "r");
+
+	std::ios::sync_with_stdio(true);
+
+	freopen_s(&CInputHandle, "CONIN$", "r", stdin);
+	freopen_s(&COutputHandle, "CONOUT$", "w", stdout);
+	freopen_s(&CErrorHandle, "CONOUT$", "w", stderr);
