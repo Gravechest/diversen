1076a26baf1d75b5cb835bb8c841f39b2ba316c7
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-08-13 10:45:32 +0200

    Update GDIdrawing.c

--- GDIdrawing.c
+++ GDIdrawing.c
@@ -1,71 +1,68 @@
 #include <windows.h>
-#include <gl/GL.h>
-#include <gl/GLU.h>
-unsigned char texture[1920][1080][4];
-const char g_szClassName[] = "myWindowClass";
+#define resX 480
+#define resY 640
+unsigned char texture[resX][resY][3];
+const char className[] = "myWindowClass";
+HWND hwnd;
+MSG Msg;
+int Ptime;
+unsigned int threadId;
+BITMAPINFO bmi = { sizeof(BITMAPINFOHEADER),resY,resX,1,24,BI_RGB };
+void WINAPI applyToScreen(HDC wdcontext) {
+	for (;;) {
+		StretchDIBits(wdcontext, 0, 0, 1920, 1080, 0, 0, resY, resX, &texture, &bmi, DIB_RGB_COLORS, SRCCOPY);
+	}
+}
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch(msg)
-    {
-        case WM_CLOSE:
-			exit(0);
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
+	switch (msg)
+	{
+	case WM_CLOSE:
+		exit(0);
+	case WM_DESTROY:
+		exit(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+	LPSTR lpCmdLine, int nCmdShow)
 {
-	BITMAPINFO bmi = {sizeof(BITMAPINFOHEADER),1920,1080,1,32,BI_RGB};
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = 0;
-    wc.lpszMenuName  = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
-	PIXELFORMATDESCRIPTOR pfd = {
-	sizeof(PIXELFORMATDESCRIPTOR), 1,
-	PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,PFD_TYPE_RGBA,
-	24,0, 0, 0, 0, 0, 0,0,0,0,
-	0,0,0,0,0,0,0,PFD_MAIN_PLANE,
-	0,0,0,0
-	};
-    RegisterClassEx(&wc);
-    hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        "raycasting",
-        WS_VISIBLE | WS_POPUP | WS_EX_TOPMOST,
-        0, 0, 1920, 1080,
-        NULL, NULL, hInstance, NULL);
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX),0,WndProc,0,0,hInstance,0,0,0,0,className,0 };
+	RegisterClassEx(&wc);
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, className, "raycasting", WS_VISIBLE | WS_POPUP | WS_EX_TOPMOST,
+		0, 0, 1920, 1080, NULL, NULL, hInstance, NULL);
 	HDC wdcontext = GetDC(hwnd);
-    UpdateWindow(hwnd);
+	UpdateWindow(hwnd);
 	ShowCursor(0);
-	SetPixelFormat(wdcontext, ChoosePixelFormat(wdcontext, &pfd), &pfd);
-	wglMakeCurrent(wdcontext, wglCreateContext(wdcontext));
-	memset(texture,100,sizeof(texture));
-    for(;;){
+	CreateThread(0, 0, applyToScreen, wdcontext, 0, &threadId);
+	float offset = 0;
+	for (;;) {
 		while (PeekMessage(&Msg, hwnd, 0, 0, 0)) {
 			GetMessage(&Msg, hwnd, 0, 0);
 			TranslateMessage(&Msg);
 			DispatchMessageW(&Msg);
 		}
-		StretchDIBits(wdcontext,0,0,1920,1080,0,0,1920,1080,&texture,&bmi,DIB_RGB_COLORS,SRCCOPY);
-    }
-    return Msg.wParam;
+		for (int i = 0; i < resX; i++) {
+			for (int i2 = 0; i2 < resY; i2++) {
+				float x = (float)i / resX / 2 - 1 + offset;
+				float y = (float)i2 / resY / 2 - 1;
+				float xa = 0;
+				float ya = 0;
+				int itt = 0;
+				while (xa + ya < 256 && itt < 256) {
+					float xtemp = xa * xa - ya * ya + x;
+					ya = 2 * xa * ya + y;
+					xa = xtemp;
+					itt++;
+				}
+				texture[i][i2][0] = itt;
+			}
+		}
+		offset += 0.01;
+	}
+	return Msg.wParam;
 }
