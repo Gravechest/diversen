fa6f326c11ef7998d3fe8be3bc61dcf80f0a7eb2
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-10-15 16:49:47 +0200

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -2,21 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-int offset = 0x000000fc;
-int sectionCount = 0;
+const int headerSz = 0x000000fc;
 int exedataSz;
 int asmSz;
-char *asm;
 int asmOffset;
+int dataOffset;
+int nameOffset;
+
+char *asm;
 char *exedata;
 
 FILE *file;
+
+typedef struct LIBRARY{
+	int namesize;
+	char *name;
+	int first;
+}LIBRARY;
+
+typedef struct FUNCTION{
+	int namesize;
+	char *name;
+	int lib;
+}FUNCTION;
+
+int libsCount;
+int funcCount;
+
+LIBRARY *libs;
+FUNCTION *func;
 
 char header[28] = {
 	0x4d,0x5a,0x00,0x00,     									//MZ
 	0x50,0x45,0x00,0x00,      									//beginning of file header
 	0x4c,0x01,													//machine type
-	0x01,0x00,													//number of sections 
+	0x02,0x00,													//number of sections 
 	0xff,0xff,0xff,0xff,										//buildDate
 	0x00,0x00,0x00,0x00,										
 	0x00,0x00,0x00,0x00,										
@@ -76,62 +96,147 @@ char linking[] = {
 	
 };
 
-void createSection(int flags,int entrypoint,int size){
+void createSection(int tsz,int dsz){
+	int flags[2] = {0x60000020,0x40000040};
 	char name[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-	switch(flags){
-		case 0x60000020:
-			memcpy(name,".text",5);
-			optHeader[4] = size;
-			optHeader[5] = size >> 8;
-			optHeader[16] = entrypoint;
-			optHeader[17] = entrypoint >> 8; 
-			optHeader[20] = entrypoint;
-			optHeader[21] = entrypoint >> 8;
-			asm = calloc(size,1);
-			asmSz = size;
-			break;
-		case 0x40000040:
-			memcpy(name,".data",5);
-			optHeader[8] = size;
-			optHeader[9] = size >> 8;
-			optHeader[24] = entrypoint;
-			optHeader[25] = entrypoint >> 8;
-			exedata = calloc(size,1);
-			exedataSz = size;
-			break;
-		default:
-			memcpy(name,".unknown",8);
-	}
-	fseek(file,offset,SEEK_SET);
+	asm = calloc(tsz,1);
+	asmSz = tsz;
+	int size = headerSz + 0x54;
+	optHeader[16] = size;
+	optHeader[17] = size >> 8; 
+	optHeader[20] = size;
+	optHeader[21] = size >> 8;
+	optHeader[4] = tsz;
+	optHeader[5] = tsz >> 8;
+	optHeader[60] = size;
+	optHeader[61] = size >> 8;
+	optHeader[56] = size + tsz + dsz;
+	optHeader[57] = (size + tsz + dsz) >> 8;
+	memcpy(name,".text",5);
+	fseek(file,headerSz,SEEK_SET);
 	fwrite(name,8,1,file);
+	fwrite(&tsz,1,4,file);
 	fwrite(&size,1,4,file);
-	fwrite(&offset,1,4,file);
+	fwrite(&tsz,1,4,file);
 	fwrite(&size,1,4,file);
-	fwrite(&offset,1,4,file);
-	fseek(file,offset + 36,SEEK_SET);
-	fwrite(&flags,1,4,file);
-	offset += 0x28;
-	sectionCount++;
+	fseek(file,12,SEEK_CUR);
+	fwrite(&flags[0],1,4,file);
+	size += tsz;
+	optHeader[8] = dsz;
+	optHeader[9] = dsz >> 8;
+	optHeader[24] = size;
+	optHeader[25] = size >> 8;
+	memcpy(name,".data",5);
+	exedata = calloc(dsz,1);
+	exedataSz = dsz;
+	dataOffset = size;
+	fwrite(name,8,1,file);
+	fwrite(&dsz,1,4,file);
+	fwrite(&size,1,4,file);
+	fwrite(&dsz,1,4,file);
+	fwrite(&size,1,4,file);
+	fseek(file,12,SEEK_CUR);
+	fwrite(&flags[1],1,4,file);	
 }
 
-void createExe(char *name){
+void createExe(char *name,int type){
 	file = fopen(name,"w");
+	optHeader[68] = type;
 }
 
 void closeExe(){
-	header[10] = sectionCount;
-	optHeader[60] = offset;
-	optHeader[61] = offset >> 8;
-	optHeader[16] = offset;
-	optHeader[17] = offset >> 8;
-	int headerSizes = offset + asmSz + exedataSz;
-	optHeader[56] = headerSizes;
-	optHeader[57] = headerSizes >> 8;
+	int adresstableSz = funcCount * 8;
+	int importdirSz = libsCount * 20 + 32;
+	if(libsCount + funcCount){
+		linking[96] = dataOffset;
+		linking[97] = dataOffset >> 8;
+		linking[100] = adresstableSz;
+		linking[101] = adresstableSz >> 8;
+		
+		dataOffset += adresstableSz * 2;
+
+		linking[8] = dataOffset;
+		linking[9] = dataOffset >> 8;
+		linking[12] = importdirSz;
+		linking[13] = importdirSz >> 8;
+		
+		dataOffset -= adresstableSz * 2;
+	}
+
+	
+	int bufOffset = dataOffset + adresstableSz * 2 + importdirSz;
+	int bufOffset2 = 0;
+	if(libsCount){
+		libs[0].first = bufOffset;
+	}
+	int libfunct = 0;
+
+	for(int i = 0;i < funcCount;i++){
+		if(libfunct != func[i].lib){
+			libfunct = func[i].lib;
+			libs[libfunct].first = bufOffset;
+		}
+		exedata[bufOffset2] = bufOffset;
+		exedata[bufOffset2 + 1] = bufOffset >> 8;
+		bufOffset2 += 8;
+	}
+	for(int i = 0;i < funcCount;i++){
+		if(libfunct != func[i].lib){
+			libfunct = func[i].lib;
+			libs[libfunct].first = bufOffset;
+		}
+		exedata[bufOffset2] = bufOffset;
+		exedata[bufOffset2 + 1] = bufOffset >> 8;
+		bufOffset2 += 8;
+		if(func[i].namesize & 1){
+			bufOffset += func[i].namesize + 5;
+		}
+		else{
+			bufOffset += func[i].namesize + 6;
+		}
+	}
+	for(int i = 0;i < libsCount;i++){
+		int rel = libs[i].first - libs[0].first + dataOffset + (funcCount * 8);
+		printf("%x",rel);
+		exedata[bufOffset2] = rel;
+		exedata[bufOffset2 + 1] = rel >> 8;
+		bufOffset2 += 12;
+		exedata[bufOffset2] = bufOffset;
+		exedata[bufOffset2 + 1] = bufOffset >> 8;
+		bufOffset2 += 4;
+		rel -= funcCount * 8;
+		exedata[bufOffset2] = rel;
+		exedata[bufOffset2 + 1] = rel >> 8;	
+		bufOffset2 += 4;
+	}
+
+	bufOffset2 += 32;
+
+	for(int i = 0;i < funcCount;i++){
+		bufOffset2 += 2;
+		memcpy(exedata + bufOffset2,func[i].name,func[i].namesize);
+		if(func[i].namesize & 1){
+			bufOffset2 += func[i].namesize + 3;
+		}
+		else{
+			bufOffset2 += func[i].namesize + 4;
+		}
+	}
+	
+	for(int i = 0;i < libsCount;i++){
+		memcpy(exedata + bufOffset2,libs[i].name,libs[i].namesize);
+		if(libs[i].namesize & 1){
+			bufOffset2 += libs[i].namesize + 1;
+		}
+		else{
+			bufOffset2 += libs[i].namesize + 2;
+		}
+	}
 	fseek(file,0,SEEK_SET);
 	fwrite(header,sizeof(header),1,file);
 	fwrite(optHeader,sizeof(optHeader),1,file);
 	fwrite(linking,sizeof(linking),1,file);
-	fseek(file,offset,SEEK_SET);
+	fseek(file,0x54,SEEK_CUR);
 	fwrite(asm,asmSz,1,file);
 	fwrite(exedata,exedataSz,1,file);
 	fclose(file);
@@ -142,13 +247,42 @@ void addAsm(char val){
 	asmOffset++;
 }
 
+void addLibrary(char *name,int nameSz){
+	libs = realloc(libs,sizeof(libs) * (libsCount + 1));
+	libs[libsCount].name = malloc(nameSz);
+	libs[libsCount].namesize = nameSz;
+	memcpy(libs[libsCount].name,name,nameSz);
+	libsCount++;
+}
+
+void addFunction(char *name,int type,int nameSz){
+	func = realloc(func,sizeof(func) * (funcCount + 1));
+	func[funcCount].lib = type;
+	func[funcCount].name = malloc(nameSz);
+	func[funcCount].namesize = nameSz;
+	memcpy(func[funcCount].name,name,nameSz);
+	funcCount++;
+}
+
 void main(){
-	createExe("gert.exe");
-	createSection(0x60000020,0x0000014c,20);
-	createSection(0x40000040,0x00000160,20);
+	createExe("gert.exe",2);
+	createSection(20,96);
+	addLibrary("USER32.dll",10);
+	addFunction("MessageBoxA",0,11);
 	addAsm(0x6a);
-	addAsm(0x10);
-	addAsm(0xeb);
-	addAsm(0xfc);
+	addAsm(0x00);
+	addAsm(0x6a);
+	addAsm(0x00);
+	addAsm(0x6a);
+	addAsm(0x00);
+	addAsm(0x6a);
+	addAsm(0x00);
+    addAsm(0xff);
+	addAsm(0x15);
+	addAsm(0x64);
+	addAsm(0x01);
+	addAsm(0x40);
+	addAsm(0x00);
+	addAsm(0xc3);
 	closeExe();
 }
