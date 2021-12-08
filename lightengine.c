48c6c3e19b5932f5b8577a5d56bcef0dea1bbf82
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-12-08 08:49:06 +0100

    Update lightengine.c

--- lightengine.c
+++ lightengine.c
@@ -82,7 +82,7 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 		}
 		int rx = cx << 9;
 		int ry = cy << 9;
-		for(int i2 = 256;i2 > 0;i2--){
+		for(;;){
 			rx += x;
 			ry += y;
 			if(rx > 0x1ffff || ry > 0x1ffff || rx < 0 || ry < 0){
@@ -91,29 +91,37 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 			switch(collision(rx,ry)){
 			case 1:
 				x = -x;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
-				bt >>= 1;
+				bt >>= 1;	
+				if(rt + gt + bt == 0){
+					goto end1;
+				}
 				break;
 			case 2:
 				y = -y;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end1;
+				}
 				break;
 			default:
 				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt,gt,rt);
 				break;
 			}
 		}
+		end1:
+			continue;
 	}
 	for(;i < 1024;i++){
 		char rt = r;
 		char gt = g;
 		char bt = b;
-		x = 512 - i;
+		x = 512 - i;	
 		y = 1024 - i;
 	    int val = mabs(mabs(x) - mabs(y));
 		for(int i2 = val;i2 < 512;i2+=5){
@@ -122,7 +130,7 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 		}
 		int rx = cx << 9;
 		int ry = cy << 9;
-		for(int i2 = 256;i2 > 0;i2--){
+		for(;;){
 			rx += x;
 			ry += y;
 			if(rx > 0x1ffff|| ry < 0 || rx < 0 || ry > 0x1ffff){
@@ -131,24 +139,31 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 			switch(collision(rx,ry)){
 			case 1:
 				x = -x;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end2;
+				}
 				break;
 			case 2:
 				y = -y;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end2;
+				}
 				break;
 			default:
 				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt,gt,rt);
 				break;
-			break;
 			}
 		}
+	end2:
+		continue;
 	}
 	for(;i < 1536;i++){
 		char rt = r;
@@ -163,7 +178,7 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 		}
 		int rx = cx << 9;
 		int ry = cy << 9;
-		for(int i2 = 256;i2 > 0;i2--){
+		for(;;){
 			rx += x;
 			ry += y;
 			if(rx < 0 || ry < 0 || rx > 0x1ffff || ry > 0x1ffff){
@@ -172,23 +187,31 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 			switch(collision(rx,ry)){
 			case 1:
 				x = -x;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end3;
+				}
 				break;
 			case 2:
 				y = -y;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end3;
+				}
 				break;
 			default:
 				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt,gt,rt);
 				break;
 			}
 		}
+	end3:
+		continue;
 	}
 	for(;i < 2048;i++){
 		char rt = r;
@@ -203,7 +226,7 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 			x++;
 			y--;
 		}
-		for(int i2 = 256;i2 > 0;i2--){
+		for(;;){
 			rx += x;
 			ry += y;
 			if(rx < 0 || ry > 0x1ff00 || rx > 0x1ffff|| ry < 0){
@@ -212,23 +235,31 @@ void light(unsigned char cx,unsigned char cy,char r,char g,char b){
 			switch(collision(rx,ry)){
 			case 1:
 				x = -x;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end4;
+				}
 				break;
 			case 2:
 				y = -y;
-				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 2,gt << 2,rt << 2);
+				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt << 3,gt << 3,rt << 3);
 				rt >>= 1;
 				gt >>= 1;
 				bt >>= 1;
+				if(rt + gt + bt == 0){
+					goto end4;
+				}
 				break;
 			default:
 				fill(((rx >> 1 & 0xff00) + (ry >> 9)) << 2,bt,gt,rt);
 				break;
 			}
 		}
+	end4:
+		continue;		
 	}
 }
 
@@ -236,20 +267,23 @@ void main(){
 	timeBeginPeriod(1);
 	heap = HeapAlloc(GetProcessHeap(),8,resX*resY*4+32*32);
 	map = heap+resX*resY*4;
-	map[32*4+10] = 1;
-	map[32*4+11] = 1;
-	map[32*4+12] = 1;
-	map[32*4+13] = 1;
-	map[32*4+14] = 1;
 	wndclass.hInstance = GetModuleHandle(0);
 	RegisterClass(&wndclass);
 	window = CreateWindowEx(0,name,name,0x90080000,0,0,resX * 4,resY * 4,0,0,wndclass.hInstance,0);
 	dc = GetDC(window);
 	for(;;){
-		light(80,80,10,1,1);
-		light(10,100,1,10,1);
-		light(40,150,1,1,10);
-		t++;
+		light(40,150,1,3,5);
+		light(50,160,2,3,4);
+		light(60,170,3,3,3);
+		light(70,180,4,3,2);
+		light(80,190,5,3,1);
+		if(GetKeyState(VK_LBUTTON) < 0){
+			POINT point;
+			GetCursorPos(&point);
+			ScreenToClient(window,&point);
+			point.y = resX * 4 - point.y;
+			map[(point.x >> 5) + (point.y & 0xffe0)] = 1;
+		}
 		StretchDIBits(dc,0,0,resX * 4,resY * 4,0,0,resX,resY,heap,&bmi,0,SRCCOPY);
 		if(PeekMessage(&Msg,window,0,0,0)){
 			GetMessage(&Msg,window,0,0);
