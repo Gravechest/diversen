d69e81511b0c6bda9ac97a7b04e0ea56ba313eda
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-12-20 14:10:13 +0100

    Update 3dEngine.c

--- OPENCL/3dEngine.c
+++ OPENCL/3dEngine.c
@@ -1,25 +1,58 @@
 #include <windows.h>
+#include <math.h>
 #include <intrin.h>
 #include <stdio.h>
 #include <cl/cl.h>
 
-#define resx 512
-#define resy 512
+#define resx 256
+#define resy 256
 
-#define res 512
+#define res 256
 
-#define MAPSZ 1 << (3 * 3)
+#define RENDERDISTANCE 32
+
+#define MAPSZ 256
+#define MAPRAM MAPSZ*MAPSZ*MAPSZ
 
 #define VRAM res*res*4
 
 #define gpu 0
 
+#define PI_2 1.57079632679489661923
+
+typedef struct RAY{
+	float x;
+	float y;
+	float z;
+	float vx;
+	float vy;
+	float vz;
+}RAY;
+
 typedef struct PLAYERDATA{
 	float xpitch;
 	float ypitch;
+
+	float xpos;
+	float ypos;
+	float zpos;
+
+	float xfov;
+	float yfov;
+
+	float xvel;
+	float yvel;
+	float zvel;
 }PLAYERDATA;
 
+typedef struct PROPERTIES{
+	int reso;
+	int lvlSz;
+	int renderDistance;
+}PROPERTIES;
+
 PLAYERDATA *player;
+PROPERTIES *properties;
 
 cl_platform_id platformid[20];
 cl_command_queue commandqueue;
@@ -27,9 +60,11 @@ cl_device_id deviceid;
 cl_context context;
 cl_program program;	
 cl_kernel kernel;
+
 cl_mem mem;
 cl_mem map_mem;
 cl_mem player_mem;
+cl_mem prop_mem;
 
 int platformC;
 int deviceC;
@@ -43,6 +78,10 @@ char *source;
 char *data;
 char *map;
 
+char settings;
+
+char threadStatus;
+
 BITMAPINFO bmi = { sizeof(BITMAPINFOHEADER),res,res,1,32,BI_RGB };
 
 const char name[] = "window";
@@ -50,22 +89,126 @@ HWND window;
 HDC dc;
 MSG Msg;
 
+
+
+void rayItterate(RAY *ray){
+	float dx,dy,dz;
+	if(ray->vx > 0){
+		dx = (int)ray->x + 1 - ray->x;
+	}
+	else{
+		dx = (int)ray->x - ray->x;
+		if(!dx){
+			dx = -1;
+		}
+	}
+	if(ray->vy > 0){
+		dy = (int)ray->y + 1 - ray->y;
+	}
+	else{
+		dy = (int)ray->y - ray->y;
+		if(!dy){
+			dy = -1;
+		}
+	}	
+	if(ray->vz > 0){
+		dz = (int)ray->z + 1 - ray->z;
+	}
+	else{
+		dz = (int)ray->z - ray->z;
+		if(!dz){
+			dz = -1;
+		}
+	}
+	if(dx / ray->vx < dy / ray->vy){
+		if(dx / ray->vx < dz / ray->vz){
+			ray->x += dx;
+			ray->y += ray->vy * dx / ray->vx;
+			ray->z += ray->vz * dx / ray->vx;
+		}
+		else{
+			ray->x += ray->vx * dz / ray->vz;
+			ray->y += ray->vy * dz / ray->vz;
+			ray->z += dz;
+		}
+	}
+	else if(dy / ray->vy < dz / ray->vz){
+		ray->x += ray->vx * dy / ray->vy;
+		ray->y += dy;
+		ray->z += ray->vz * dy / ray->vy;
+	}
+	else{
+		ray->x += ray->vx * dz / ray->vz;
+		ray->y += ray->vy * dz / ray->vz;
+		ray->z += dz;
+	}
+}
+
 long _stdcall proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	switch(msg){
 	case WM_KEYDOWN:
-		if(GetKeyState(VK_UP) & 0x80){
-			player->ypitch += 0.3;
-		}
-		else if(GetKeyState(VK_DOWN) & 0x80){
-			player->ypitch -= 0.3;
-		}
-		else if(GetKeyState(VK_LEFT) & 0x80){
-			player->xpitch -= 0.3;
-		}
-		else if(GetKeyState(VK_RIGHT) & 0x80){
-			player->xpitch += 0.3;
+		if(GetKeyState(VK_F1) & 0x80){
+			ShowCursor(settings & 1);
+			settings ^= 1;
+			if(settings & 1){
+				SetCursorPos(50,50);
+			}
 		}
 		break;
+	case WM_MOUSEMOVE:;
+		if(settings & 1){
+			POINT curp;
+			GetCursorPos(&curp);
+			player->xpitch += (float)(curp.x - 50) / 100;
+			player->ypitch -= (float)(curp.y - 50) / 100;
+			if(player->ypitch < -2){
+				player->ypitch = -2;
+			}
+			SetCursorPos(50,50);
+		}
+		break;
+	case WM_LBUTTONDOWN:{
+			RAY ray;
+			ray.x = player->xpos;
+			ray.y = player->ypos;
+			ray.z = player->zpos;
+			ray.vz = sin((float)(res/2) / res / player->yfov + player->ypitch);
+			ray.vx = cos((float)(res/2) / res / player->xfov + player->xpitch) * cos((float)(res/2) / res / player->yfov + player->ypitch);
+			ray.vy = sin((float)(res/2) / res / player->xfov + player->xpitch) * cos((float)(res/2) / res / player->yfov + player->ypitch);
+			for(int i = 0;i < 8;i++){
+				rayItterate(&ray);
+				if(map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz]){
+					if(ray.z - (int)ray.z == 0){
+						map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz - properties->lvlSz * properties->lvlSz] = 1;
+					}
+					else if(ray.y - (int)ray.y == 0){
+						map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz - properties->lvlSz] = 1;
+					}
+					else{
+						map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz - 1] = 1;
+					}
+					break;
+				}
+			}
+			break;
+		}
+	case WM_RBUTTONDOWN:{
+			RAY ray;
+			ray.x = player->xpos;
+			ray.y = player->ypos;
+			ray.z = player->zpos;
+			ray.vz = sin((float)(res/2) / res / player->yfov + player->ypitch);
+			ray.vx = cos((float)(res/2) / res / player->xfov + player->xpitch) * cos((float)(res/2) / res / player->yfov + player->ypitch);
+			ray.vy = sin((float)(res/2) / res / player->xfov + player->xpitch) * cos((float)(res/2) / res / player->yfov + player->ypitch);
+			for(int i = 0;i < 8;i++){
+				rayItterate(&ray);
+				if(map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz]){
+					map[(int)ray.x + (int)ray.y * properties->lvlSz + (int)ray.z * properties->lvlSz * properties->lvlSz] = 0;
+					break;
+				}
+			}
+			break;
+		}
 	}
 	return DefWindowProc(hwnd,msg,wParam,lParam);
 }
@@ -73,22 +216,27 @@ long _stdcall proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 WNDCLASS wndclass = {0,proc,0,0,0,0,0,0,name,name};
 
 void openCL(){
-	clEnqueueWriteBuffer(commandqueue,map_mem,1,0,MAPSZ,map,0,0,0);
+	long long t = _rdtsc();	
+	clEnqueueWriteBuffer(commandqueue,map_mem,1,0,MAPRAM,map,0,0,0);
 	clEnqueueWriteBuffer(commandqueue,player_mem,1,0,sizeof(PLAYERDATA),player,0,0,0);
+	clEnqueueWriteBuffer(commandqueue,prop_mem,1,0,sizeof(PROPERTIES),properties,0,0,0);
 	clEnqueueWriteBuffer(commandqueue,mem,1,0,VRAM,data,0,0,0);
 	clEnqueueNDRangeKernel(commandqueue,kernel,1,0,&count,&count2,0,0,0);
 	clEnqueueReadBuffer(commandqueue,mem,1,0,VRAM,data,0,0,0);
 }
 
 void main(){
+	timeBeginPeriod(1);
+
 	HANDLE h = CreateFile("source.cl",GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
 	fsize = GetFileSize(h,0);
 	source = HeapAlloc(GetProcessHeap(),8,fsize);
 	ReadFile(h,source,fsize,0,0);
 
-	data   = HeapAlloc(GetProcessHeap(),8,VRAM);
-	map    = HeapAlloc(GetProcessHeap(),8,MAPSZ);
-	player = HeapAlloc(GetProcessHeap(),8,sizeof(PLAYERDATA));
+	data       = HeapAlloc(GetProcessHeap(),8,VRAM);
+	map        = HeapAlloc(GetProcessHeap(),8,MAPRAM);
+	player     = HeapAlloc(GetProcessHeap(),8,sizeof(PLAYERDATA));
+	properties = HeapAlloc(GetProcessHeap(),8,sizeof(PROPERTIES));
 
 	clGetPlatformIDs(20,platformid,&platformC);
 	clGetDeviceIDs(platformid[gpu],CL_DEVICE_TYPE_DEFAULT,1,&deviceid,0);
@@ -97,35 +245,124 @@ void main(){
 	program = clCreateProgramWithSource(context,1,(const char**)&source,0,0);
 	clBuildProgram(program,0,0,0,0,0);
 	kernel = clCreateKernel(program,"add",0);
-	if(!kernel){
+	if(!kernel){	
 		printf("program has not compiled\n");
-		exit(0);
+		ExitProcess(0);
 	}
 
 	mem        = clCreateBuffer(context,0,VRAM,0,0);
-	map_mem    = clCreateBuffer(context,1,MAPSZ,0,0);
-	player_mem = clCreateBuffer(context,2,sizeof(PLAYERDATA),0,0);
+	map_mem    = clCreateBuffer(context,0,MAPRAM,0,0);
+	player_mem = clCreateBuffer(context,0,sizeof(PLAYERDATA),0,0);
+	prop_mem   = clCreateBuffer(context,0,sizeof(PROPERTIES),0,0);
 
 	clSetKernelArg(kernel,0,sizeof(mem),(void*)&mem);
 	clSetKernelArg(kernel,1,sizeof(map_mem),(void*)&map_mem);
 	clSetKernelArg(kernel,2,sizeof(player_mem),(void*)&player_mem);
+	clSetKernelArg(kernel,3,sizeof(prop_mem),(void*)&prop_mem);
 
 	wndclass.hInstance = GetModuleHandle(0);
 	RegisterClass(&wndclass);
-	window = CreateWindowEx(0,name,name,0x10080000,0,0,resy + 16,resx + 39,0,0,wndclass.hInstance,0);
+	window = CreateWindowEx(0,name,name,0x90080000,0,0,resy + 16,resx + 39,0,0,wndclass.hInstance,0);
 	dc = GetDC(window);
 
-	for(int i = 0;i < MAPSZ;i++){
-		if(!_rdtsc() & 15){
-			map[i] = 1;
-		}
-	}
+	for(int i = 0;i < VRAM;i+=20){
+		map[i] = 1;
+	}	
+	clEnqueueWriteBuffer(commandqueue,map_mem,1,0,MAPRAM,map,0,0,0);
+
+	player->xfov = 0.5;
+	player->yfov = 1;
+	player->zpos = 4.5;
+	player->xpos = 6;
+	player->ypos = 6;
+
+	properties->lvlSz = MAPSZ;
+	properties->renderDistance = RENDERDISTANCE;
+	properties->reso = res*4;
+
+	settings = 1;
+	ShowCursor(0);
 
 	for(;;){
-		long long timer = _rdtsc();
-		openCL();
-		printf("%i\n",_rdtsc() - timer >> 22);
-		StretchDIBits(dc,0,0,resx,resy,0,0,res,res,data,&bmi,0,SRCCOPY);
+		if(GetKeyState(0x57) & 0x80){
+			player->xvel += cosf(player->xpitch + 0.50 / player->xfov) / 120;
+			player->yvel += sinf(player->xpitch + 0.50 / player->xfov) / 120;
+		}
+		if(GetKeyState(0x53) & 0x80){
+			player->xvel -= cosf(player->xpitch + 0.50 / player->xfov) / 120;
+			player->yvel -= sinf(player->xpitch + 0.50 / player->xfov) / 120;
+		}
+		if(GetKeyState(0x44) & 0x80){
+			player->xvel += cosf(player->xpitch + 0.50 / player->xfov + PI_2) / 120;
+			player->yvel += sinf(player->xpitch + 0.50 / player->xfov + PI_2) / 120;
+		}
+		if(GetKeyState(0x41) & 0x80){
+			player->xvel -= cosf(player->xpitch + 0.50 / player->xfov + PI_2) / 120;
+			player->yvel -= sinf(player->xpitch + 0.50 / player->xfov + PI_2) / 120;
+		}
+		if(!settings & 0x01){
+			if(GetKeyState(VK_UP) & 0x80){
+				player->ypitch += 0.03;
+			}
+			if(GetKeyState(VK_DOWN) & 0x80 && player->ypitch > -1){
+				player->ypitch -= 0.03;
+			}
+			if(GetKeyState(VK_LEFT) & 0x80){
+				player->xpitch -= 0.03;
+			}
+			if(GetKeyState(VK_RIGHT) & 0x80){
+				player->xpitch += 0.03;
+			}
+		}
+		if(player->zpos > 1.2){
+			player->zvel -= 0.02;
+		}
+		else{
+			player->zvel = 0;
+			if(GetKeyState(VK_SPACE) & 0x80){
+				player->zvel = 0.4;	
+			}
+		}
+		player->xpos += player->xvel;
+		player->ypos += player->yvel;
+		player->zpos += player->zvel;
+		if(map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ]
+		|| map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ + 64]){
+			player->xpos -= player->xvel;
+			if(map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ]
+			|| map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ + 64]){
+				player->xpos += player->xvel;
+				player->ypos -= player->yvel;
+				if(map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ]
+				|| map[(int)player->xpos + (int)player->ypos*MAPSZ + (int)player->zpos*MAPSZ*MAPSZ + 64]){
+					player->ypos += player->yvel;
+					player->zpos -= player->zvel;
+					if(GetKeyState(VK_SPACE) & 0x80){
+						player->zvel = 0.4;	
+					}
+					else{
+						player->zvel = 0;
+					}
+				}
+				else{
+					player->yvel = 0;
+				}
+			}
+			else{
+				player->xvel = 0;
+			}
+		}
+		player->xvel /= 1.07;
+		player->yvel /= 1.07;
+		player->zvel /= 1.07;
+
+		HANDLE renderThread = CreateThread(0,0,openCL,0,0,0);
+		Sleep(15);
+		WaitForSingleObject(renderThread,9999);
+		for(int i = 0;i < 10;i++){
+			data[res/2 * res * 4 + res/2 * 4 + i] = 255;
+		}
+		StretchDIBits(dc,0,0,resy,resx,0,0,res,res,data,&bmi,0,SRCCOPY);
 		memset(data,0,VRAM);
 		while(PeekMessage(&Msg,window,0,0,0)){
 			GetMessage(&Msg,window,0,0);
