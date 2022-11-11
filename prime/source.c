4cd668273741fedede9dc8f942b78ce97500c512
Gravechest <75589056+Gravechest@users.noreply.github.com>
2022-11-11 12:04:48 +0100

    Update source.c

--- prime/source.c
+++ prime/source.c
@@ -1,26 +1,44 @@
-#include <stdio.h>
+#include <windows.h>
+
+int strC;
+char str[1000];
+
+void u32ToStr(int p){
+    int sz = 0;
+	for(int i = 1;i <= p;i*= 10){
+		sz++;
+	}
+	for(int i = 0,div = 1;i < sz;i++,div*=10){
+		str[strC+sz-i] = p / div % 10 + 0x30;
+	}
+	str[strC+sz+1] = '\n';
+    strC+=sz+1;
+}   
 
 void main(){
-    int n1 = 3;
-    int n2 = 2;
-    long long priem = 1;
-
-    while(1)
-    {
-        while(n1 > n2)
-        {
-            if(n1 % n2 == 0)
-            {
-                priem = 0;
+    HANDLE ch = GetStdHandle(STD_OUTPUT_HANDLE);
+    for(int num = 0;;num++){
+        if (num == 2 || num == 3){
+			u32ToStr(num);
+            if(strC > 200){
+                WriteConsoleA(ch,str,strC+1,0,0);
+                strC = 0;
             }
-            n2++;
         }
-        if(priem)
-        {
-            printf("%i\n",n1);
+        if (num <= 1 || num % 2 == 0 || num % 3 == 0){
+            continue;
         }
-        n1++;
-        n2 = 2;
-        priem = 1;
+        for (int check = 5; check * check <= num; check += 6){
+            if (num % check == 0 || num % (check + 2) == 0){
+                 goto con;
+            }
+        }
+		u32ToStr(num);
+        if(strC > 200){
+            WriteConsoleA(ch,str,strC+1,0,0);
+            strC = 0;
+        }
+    con:
+        continue;
     }
 }
